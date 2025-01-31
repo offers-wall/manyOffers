d9055c684d390811698ce2aa0d3e016470a16c7c
ArtemLizantan <plishkan25@gmail.com>
2025-01-31 09:48:18 +0200

    fix

--- src/js/prelends/aviator3.js
+++ src/js/prelends/aviator3.js
@@ -1,16 +1,5 @@
 export const aviator3 = `<section class="aviatorA3">
-  <img class="aviatorA3__logo" src="./images/aviator3/logo.svg" alt="" />
-  <img
-    class="aviatorA3__airplane"
-    src="./images/aviator3/airplane.png"
-    alt=""
-  />
-  <img class="aviatorA3__arrows" src="./images/aviator3/arrows.png" alt="" />
-  <img
-    class="aviatorA3__arrows-mobile"
-    src="./images/aviator3/mobileArrows.png"
-    alt=""
-  />
+   <img class="aviatorA3__bg" />
   <div class="aviatorA3__popup popup-aviatorA3">
     <div class="popup-aviatorA3__content">
       <h1 class="popup-aviatorA3__title">Sizin 18 yaşınız var?</h1>
